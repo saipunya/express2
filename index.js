be06edf4e876eb4c4dfec54fb1d133397cde4d35
saipunya <sumetchoorat@gmail.com>
2025-01-09 20:36:55 +0700

    update09012568

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const path = require('path')
+// app.use(express.static('public'))
+app.set('views','./views')
+app.set('view engine', 'ejs')
+app.get('/',(req, res) => {
+    res.send('welcome to Naimet website!')
+
+  })
+  app.get('/person',(req, res) => {
+    res.render('person', {
+       name :{
+        firstName : 'sumet',
+        lastName : 'choorat'
+       },
+       address : {
+        city : 'chaiyaphum',
+        country : 'bangkok'
+       },children : ['Micky','Mint']
+    })
+})
+app.listen(port, ()=>{
+    console.log('object listening on port 5000');
+})
