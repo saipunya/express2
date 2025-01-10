c218b9280059d4889a5d2eb145cf733933d818e1
saipunya <sumetchoorat@gmail.com>
2025-01-10 17:21:22 +0700

    update10012568

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path')
+const ejs = require('ejs');
 // app.use(express.static('public'))
 app.set('views','./views')
 app.set('view engine', 'ejs')
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
 app.get('/',(req, res) => {
-    res.send('index',{
-      title : 'Welcome to website'
-    })
+    res.render('index')
 
   })
   app.get('/person',(req, res) => {
