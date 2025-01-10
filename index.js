34919f8e6f231e37e8dfaa13e0f9ea76939d4ff1
saipunya <sumetchoorat@gmail.com>
2025-01-10 20:39:26 +0700

    update10012568

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path')
-const ejs = require('ejs');
+const ejs = require('ejs')
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
 // app.use(express.static('public'))
+
 app.set('views','./views')
 app.set('view engine', 'ejs')
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.json())
+
 app.get('/',(req, res) => {
     res.render('index')
 
