cc824bcc89a95f16234d872e4470849199c6cc4e
saipunya <sumetchoorat@gmail.com>
2025-02-06 15:33:24 +0700

    update06022568

--- Routes/homeRoute.js
+++ Routes/homeRoute.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const connection = require('../database')
 
 
 router.get('/', (req, res) => {
+   // Check user in database
+   connection.query('SELECT * FROM download ORDER BY down_savedate DESC LIMIT 20', async (err, result) => {
+    if (err) throw err;
+
+
     res.render('index',
       {
         message: req.flash('error'),
-         user: req.session.user || ''
+         user: req.session.user || '',
+         data : result
        }
     );
+
+  });
+
   });
   module.exports = router
